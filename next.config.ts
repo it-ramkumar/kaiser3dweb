2149955bd3b5eeffc81b44169b8870a6efa263e3
ramkumar <ramkumartirole@gmail.com>
2025-12-09 22:35:29 +0530

    added env in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 1. Force these variables to be available to the app
+  env: {
+    ADMIN_USER: process.env.ADMIN_USER,
+    ADMIN_PASS: process.env.ADMIN_PASS,
+  },
 };
 
 export default nextConfig;
